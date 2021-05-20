02c02ffef89484df8c78bb1a84ecac922433ae99
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-05-20 22:52:03 +0900

    Initial Commit

---
+++ ARCameraTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ARCameraTest
+//
+//  Created by 福島大稀 on 2021/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
